77f2a8be3c45e067eb178bcfdfbb341bbceb1e6d
marcio <marcioqui3@gmail.com>
2022-02-11 16:10:24 +0100

    dkjf

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Adimin</title>
+        <title>Emarpe Accounting</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
