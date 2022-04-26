dff91b7dde5ff8120238d33754cc28d03d7bb943
marcio <marcioqui3@gmail.com>
2022-04-26 15:17:55 +0100

    jkk

--- pages/dummy.js
+++ pages/dummy.js
@@ -18,7 +18,7 @@ export default function Dummy() {
 
         <div className="row">
           <div className="col-md-12">
-            <h3 className="mt-3 mb-3">Dummmy</h3>
+            <h3 className="mt-3 mb-3">Dummmmmy</h3>
             <h1>haha</h1>
           </div>
         </div>
